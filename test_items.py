8815a40cee11e02d5b3c80cf1e38cbbc6ef3d082
Test User <none@email.com>
2020-08-10 18:32:10 +0200

    init commit. solved task.

--- test_items.py
+++ test_items.py
@@ -1,10 +1,9 @@
 import time
 
-
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_find_basket(browser):
+def test_find_btn_add_to_basket(browser):
     browser.get(link)
     time.sleep(30)
-    browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
+    assert len(browser.find_elements_by_css_selector("#add_to_basket_form")) > 0, "корзинка не найдена"
