0d02597b9b9ad0a7129ddd93d6b0f67f661dd755
suminv <43213479+suminv@users.noreply.github.com>
2020-08-10 16:35:03 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,1 +1,6 @@
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+
+def test_find_basket(browser):
+    browser.get(link)
+    browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
